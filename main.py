1ad1215dd6f6423e29581167b16f0dab8de324d3
ZukoMcTaco <112821163+ZukoMcTaco@users.noreply.github.com>
2024-03-26 03:59:42 +0000

    change host name to netlify.app

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ app=create_app()
 
 if __name__=='__main__':
 
-    app.run(host="192.168.1.200",port="5000",threaded=True,debug=True)
+    app.run(host="https://master--incredible-rugelach-e6fe4b.netlify.app",port="5000",threaded=True,debug=True)
     #runs the website
